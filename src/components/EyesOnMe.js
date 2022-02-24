e6f15c0662f3f2954989853c903b179970d9969b
Tunisia Artope <artope@yahoo.com>
2022-02-24 16:36:12 -0500

    commit tree bf4aa176378e17dc406a72004f6000dde618f04e

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    
+    function watching(){
+        console.log('Good!')
+    }
+
+    function notWatching(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return(
+        <div>
+            <button onFocus={watching} onBlur={notWatching}>'Eyes on me'</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    function keyPadChange(){
+        console.log("Entering password...")
+    }
+    return (
+        <div>
+            <input 
+                type="password"
+                onChange={keyPadChange}
+            />
+        </div>
+    )
+}
+
+
+
+
+
+export default Keypad;
